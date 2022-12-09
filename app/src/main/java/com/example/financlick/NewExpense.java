9dae48901c06e975026704a02e89a3c5b849ef72
William Basso <williambasso12@gmail.com>
2022-12-08 21:00:47 -0300

    feat: adicionar metodo de nova despesa

--- app/src/main/java/com/example/financlick/NewExpense.java
+++ app/src/main/java/com/example/financlick/NewExpense.java
@@ -1,14 +1,61 @@
 package com.example.financlick;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewExpense extends AppCompatActivity {
+    private TextView expenseDescription, expenseValue, expenseDate, expenseCategory, expenseAmount;
+    private Switch expenseFixed, expenseRecurrent;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_expense);
+
+        expenseDescription = findViewById(R.id.expenseDescription);
+        expenseValue = findViewById(R.id.expenseValue);
+        expenseDate = findViewById(R.id.expenseDate);
+        expenseCategory = findViewById(R.id.expenseCategory);
+        expenseFixed = findViewById(R.id.expenseFixed);
+        expenseRecurrent = findViewById(R.id.expenseRecurrent);
+        expenseAmount = findViewById(R.id.expenseAmount);
+    }
+
+    public void addExpense(View view) {
+        Map<String, Object> expense = new HashMap<>();
+        expense.put("description", expenseDescription.getText().toString());
+        expense.put("value", Double.parseDouble(expenseValue.getText().toString()));
+        expense.put("date", expenseDate.getText().toString());
+        expense.put("category", expenseCategory.getText().toString());
+        expense.put("fixed", expenseFixed.isChecked());
+        expense.put("recurrent", expenseRecurrent.isChecked());
+        expense.put("amount", Integer.parseInt(expenseAmount.getText().toString()));
+
+        db.collection("expenses").add(expense).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(), "Despesa adicionada com sucesso", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Erro ao adicionar despesa", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
